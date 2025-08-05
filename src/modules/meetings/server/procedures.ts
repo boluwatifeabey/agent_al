f9f625dd4853a0310fd0a65d2c3e894584af314c
otulaja favour <otulajafavour14@gmail.com>
2025-08-05 14:29:12 +0100

    20: Meetings Page

--- src/modules/meetings/server/procedures.ts
+++ src/modules/meetings/server/procedures.ts
@@ -9,7 +9,26 @@ import { meetingsInsertSchema, meetingsUpdateSchema } from "../schemas";
 import { MeetingStatus } from "../types";
 
 export const meetingsRouter = createTRPCRouter({
-
+    remove: protectedProcedure
+        .input(z.object({ id: z.string()}))
+        .mutation(async ({ ctx, input}) => {
+            const [removeMeeting] = await db
+                .delete(meetings)
+                .where(
+                    and (
+                        eq(meetings.id, input.id),
+                        eq(meetings.userId, ctx.auth.user.id),
+                    )
+                )
+                .returning();
+            if (!removeMeeting) {
+                throw new TRPCError({
+                    code: "NOT_FOUND",
+                    message: "Meeting not found"
+                })
+            }
+        return removeMeeting;
+    }),
     update: protectedProcedure
         .input(meetingsUpdateSchema)
         .mutation(async ({ ctx, input}) => {
@@ -56,8 +75,11 @@ export const meetingsRouter = createTRPCRouter({
             .select({
                 //todo change to actual count
                 ...getTableColumns(meetings),
+                agent: agents,
+                duration: sql<number>`EXTRACT(EPOCH FROM (ended_at - started_at))`.as("duration"),
             })
             .from(meetings)
+            .innerJoin(agents, eq(meetings.agentId, agents.id))
             .where(
                 and(
                     eq(meetings.id, input.id),
